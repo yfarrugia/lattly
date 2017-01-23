94462cabe8ca76a7756ccb9b94554065f132fe39
yfarrugia <yfarrugia@gmail.com>
2017-01-23 21:49:40 +0000

    updated

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-__author__ = 'yanikafarrugia'
-
-from setuptools import setup, find_packages
-
-with open('README.md') as f:
-	readme = f.read()
-with open('LICENSE') as f:
-	license = f.read()
-
-setup(
-	name = 'lattly',
-	version = '0.1.0',
-	description = 'Bringing People Together',
-	long_description = readme,
-	author = 'Yanika Farrugia',
-	author_email = 'farrugia.yani@gmail.com',
-	url = 'https://github.com/yfarrugia/lattly',
-	license = license,
-	packages = find_packages(exclude = ('lattly_tests', 'lattly_docs'))
-)
+# __author__ = 'yanikafarrugia'
+#
+# from setuptools import setup, find_packages
+#
+# with open('README.md') as f:
+# 	readme = f.read()
+# with open('LICENSE') as f:
+# 	license = f.read()
+#
+# setup(
+# 	name = 'lattly',
+# 	version = '0.1.0',
+# 	description = 'Bringing People Together',
+# 	long_description = readme,
+# 	author = 'Yanika Farrugia',
+# 	author_email = 'farrugia.yani@gmail.com',
+# 	url = 'https://github.com/yfarrugia/lattly',
+# 	license = license,
+# 	packages = find_packages(exclude = ('lattly_tests', 'lattly_docs'))
+# )
